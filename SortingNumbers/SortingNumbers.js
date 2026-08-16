2983299ab6971ed4e4ad51e198df668eb8ef2411
sujanaankem <ankemsujana@gmail.com>
2026-08-16 18:25:30 +0530

    Sorting numbers

--- SortingNumbers/SortingNumbers.js
+++ SortingNumbers/SortingNumbers.js
@@ -1,6 +1,5 @@
 let array = [];
 function onClickPush(){
-    debugger;
     let numbers = Number(document.getElementById("txtNumbers").value);
     array.push(numbers);
     document.getElementById("pPush").innerHTML = array;
@@ -8,16 +7,32 @@ function onClickPush(){
 }
 
 function onClickSort(){
+     debugger;
     let sort = array.sort((a,b) => a-b);
     document.getElementById("pSort").innerHTML = sort;
 }
 
 function onClickLoop(){
+     debugger;
     let target = 9;
     let left = 0;
     let rigth = array.length -1;
-    let sumCount = [];
-    while {
-        
+    let sum = [];
+    let count = 0;
+    while (left<rigth){
+        sum = (array[left]+array[rigth])
+        if(sum == target){
+            left ++;
+            rigth --;
+            count ++;
+        }
+        else if(sum > target){
+            rigth --;
+        }
+        else if(sum < target){
+            left ++;
+        }
     }
+    document.getElementById("pCount").innerHTML = count;
+   // document.getElementById("pSum").innerHTML = sum;
 }
